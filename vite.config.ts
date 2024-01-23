35f2b0d282eabe3f6dd76c7eef0098b4476cc9f0
asobolev <asobolev@merchantgrowth.com>
2024-01-23 11:33:55 -0800

    feat: dockerized app and added to pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+  },
 });
